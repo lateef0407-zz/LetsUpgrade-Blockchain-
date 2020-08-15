44c356829beb56dd75cf9ca067ad570c77a51dab
lateef0407 <63701286+lateef0407@users.noreply.github.com>
2020-08-15 14:49:36 +0530

    Create employees.sol

---
+++ employees.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.17 <0.6.12;
+contract employeeDetails {
+    string public name;
+    uint public salary;
+    uint public mobno;
+    string public emplyid;
+    string public designation;
+    
+   
+    constructor(string ename,uint sal,uint emobno,
+                             string empid,string edesignation) public {
+       
+        name=ename;
+        mobno=emobno;
+       emplyid=empid;
+        designation=edesignation;
+    }
+   
+    function getCrrentDetails() public view returns(string ,uint ,
+                            uint, string ,string ) {
+        return (name,salary,mobno,emplyid, designation);
+    }
+   
+}
+
+
+The contract is deployed at
+https://ropsten.etherscan.io/address/0xbab4acbe1c012acc1e7689af3d3f8f49bbc1e35d
+and address is 0xBAB4aCBE1C012ACC1E7689Af3d3F8f49BBC1e35E
